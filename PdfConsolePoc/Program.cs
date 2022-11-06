4e0c430807a2aadf09eea90c4bf707504e401353
Dick Kluis <dick@concise-advice.net>
2022-11-06 06:25:37 -0500

    Trying to change a textbox value

--- PdfConsolePoc/Program.cs
+++ PdfConsolePoc/Program.cs
@@ -16,7 +16,38 @@ namespace PdfConsolePoc
             for (var i = 0; i < formWidget.FieldsWidget.List.Count; i++)
             {
                 var field = formWidget.FieldsWidget.List[i] as PdfField;
-                Console.WriteLine($"Field {i} : {formWidget.FieldsWidget.List[i]} with TextBox : {field.Name}");
+                if (field is PdfTextBoxFieldWidget textBoxField)
+                {
+                    if (textBoxField.Name.Contains("Given"))
+                    {
+                        continue;
+                    }
+                    var name = textBoxField.Name;
+                    var misc = textBoxField.Text;
+                    Console.WriteLine($"Type: TextBox, Name: {name}, Text: {misc}");
+                    textBoxField.Text = "Hello";
+                    Console.WriteLine($"Type: Text, Name: {name}, Text: {textBoxField.Text}");
+
+                }  
+                else if (field is PdfCheckBoxWidgetFieldWidget checkBoxField)
+                {
+                    var name = checkBoxField.Name;
+                    var misc = checkBoxField.Checked;
+                    Console.WriteLine($"Type: Check, Name: {name}, Checked: {misc}");
+                }
+                else if (field is PdfListBoxWidgetFieldWidget listBoxField)
+                {
+                    var name = listBoxField.Name;
+                    var misc = listBoxField.Items;
+                    Console.WriteLine($"Type: List, Name: {name}, Checked: {misc}");
+                }
+                else if (field is PdfComboBoxWidgetFieldWidget comboBoxField)
+                {
+                    var name = comboBoxField.Name;
+                    var misc = comboBoxField.SelectedValue;
+                    Console.WriteLine($"Type: Combo, Name: {name}, Checked: {misc}");
+                }
+                //Console.WriteLine($"Field {i} : {formWidget.FieldsWidget.List[i]} with TextBox : {field.Name}");
             }
         }
     }
