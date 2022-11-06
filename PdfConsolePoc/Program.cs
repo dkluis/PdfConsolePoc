8003fa5af341ca8910130bb5f72da4debc7d08cb
Dick Kluis <dick@concise-advice.net>
2022-11-05 20:39:55 -0500

    1st commit PDF POC stuff

--- PdfConsolePoc/Program.cs
+++ PdfConsolePoc/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace PdfConsolePoc
+﻿using System;
+using Spire.Pdf;
+using Spire.Pdf.Fields;
+using Spire.Pdf.Widget;
+
+namespace PdfConsolePoc
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal static class Program
     {
+        public static void Main(string[] args)
+        {
+            PdfDocument doc = new PdfDocument();
+            doc.LoadFromFile(@"/Users/dick/Desktop/SampleForm-1.pdf");
+
+            var formWidget = doc.Form as PdfFormWidget;
+            for (var i = 0; i < formWidget.FieldsWidget.List.Count; i++)
+            {
+                var field = formWidget.FieldsWidget.List[i] as PdfField;
+                Console.WriteLine($"Field {i} : {formWidget.FieldsWidget.List[i]} with TextBox : {field.Name}");
+            }
+        }
     }
-  }
 }
